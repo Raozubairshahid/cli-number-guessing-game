ae2acf8a9449091af54c12e8b0848d14e7f5f946
Rao Muhammad Zubair <zubairrao23@gmail.com>
2024-05-21 22:47:33 +0500

    added a console and change the version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import inquirer from "inquirer";
 // 2) User input for guessing number - Done.
 // 3) Compare user input with computer generated number and show result - Done.
 const randomNumber = Math.floor(Math.random() * 6 + 1);
-console.log(randomNumber);
+console.log("Welcome to number guessing game");
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
